0bebd9cd9f0ab424df2c4f197511813e0f7c11b0
jshannon75 <jshannon@uga.edu>
2017-10-11 12:18:02 -0400

    Week 10 stuff

--- Class_scripts/Geog4300_Fa17_Lecture 9-2 Data wrangling.R
+++ Class_scripts/Geog4300_Fa17_Lecture 9-2 Data wrangling.R
@@ -37,3 +37,20 @@ degree_data<-data.frame(cbind(degree,year,admit))
 #Let's tidy this long data using spread
 degree_data_wide<-degree_data %>%
   spread(year,admit)
+
+#We can also use spread to facet plots with ggplot and tmap
+ggplot(acsdata_long,aes(x=value))+
+  geom_histogram()+
+  facet_wrap(~var)
+
+library(sf)
+library(tmap)
+acsdata_sf<-st_read("https://github.com/jshannon75/geog4300/raw/master/Data/ACSCtyDat_2014ACS_simplify.geojson") %>%
+  select(c(CTY_NAME,State,WHT_POP_PC,AFAM_POP_P,ASN_POP_PC,AMIN_POP_P,POV_POP_PC,LESSHS_PCT)) %>%
+  filter(State!="Alaska",State!="Hawaii")
+acsdata_sf_long<-acsdata_sf %>%
+  gather(key="var",value="value",-CTY_NAME,-State,-geometry)
+
+tm_shape(acsdata_sf_long)+
+  tm_fill("value")+
+  tm_facets("var",free.coords=TRUE,free.scales.fill=TRUE)
